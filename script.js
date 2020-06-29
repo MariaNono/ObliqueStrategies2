d37d9357bd4519e9b1184da2c3e078111e27c8d4
Maria <maria@brainbot.com>
2020-06-29 10:58:31 +0200

    implement basic functionality

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-const URL = 'http://brianeno.needsyourhelp.org/draw'
 const h1 = document.getElementById('strategy')
+let result = []
 
 const change = (strategy) => {
     h1.innerHTML = strategy
 }
 
-const getStrategy = () => {
-    fetch(proxyUrl + URL)
-        .then(response => response.json())
-        .then((data) => {
-            console.log(data["strategy"])
-            change(data['strategy'])
-        })
+const makeArray = text => {
+    const strategiesArray = text.split('\n')
+    return strategiesArray
 }
 
-document.addEventListener('DOMContentLoaded', getStrategy())
+async function getStrategies() {
+    const response = await fetch('strategies.txt')
+    const text = await response.text()
+    return makeArray(text)
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    result = await getStrategies()
+    console.log(result)
+})
 
 document.defaultView.addEventListener('click', () => {
-    console.log('click')
-    getStrategy()
+    const rand = Math.floor(Math.random()*result.length)
+    if (result.length === 0) return
+    change(result[rand])
 })
