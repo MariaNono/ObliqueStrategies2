3ea396a2d5701876f0ef101565cd3384b7728bd4
Maria <maria@brainbot.com>
2020-09-23 17:30:07 +0200

    add functionality to go backwards with bottom arrow key

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const h1 = document.getElementById('strategy')
 let result = []
+let drawn = []
+let index = -1
 
 const change = (strategy) => {
     h1.innerHTML = strategy
+}
+
+const randomize = () => {
+    const rand = Math.floor(Math.random()*result.length)
+    change(result[rand])
+    drawn.push(result[rand])
+    result.splice(rand, 1)
+    index++
 }
 
 const makeArray = text => {
@@ -19,10 +29,29 @@ async function getStrategies() {
 document.addEventListener('DOMContentLoaded', async () => {
     result = await getStrategies()
     console.log(result)
+    randomize()
 })
 
-document.defaultView.addEventListener('click', () => {
-    const rand = Math.floor(Math.random()*result.length)
+document.defaultView.addEventListener('keyup', (e) => {
     if (result.length === 0) return
-    change(result[rand])
+    if (e.keyCode === 38) {
+        if (index < (drawn.length - 1)) {
+            index++
+            change(drawn[index])
+        } else {
+            randomize()
+        }
+    }
+    console.log(drawn)
+    console.log(result.length)
 })
+
+document.defaultView.addEventListener('keyup', (e) => {
+    if (index === 0) return
+    if (e.keyCode === 40) {
+        index--
+        change(drawn[index])
+    }
+})
+
+// HIDE MOUSE?
