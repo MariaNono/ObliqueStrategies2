ec72ce43d1fcc9bc1a8a713707fa372162d82abd
Maria Nogueira <leo.ds.nogueira@gmail.com>
2020-09-27 16:12:03 +0200

    add card container, transition, info div, instructions

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const h1 = document.getElementById('strategy')
+const card = document.getElementById('card')
 let result = []
 let drawn = []
 let index = -1
+let degrees = 0
 
 const change = (strategy) => {
-    h1.innerHTML = strategy
+    setTimeout(() => { 
+        h1.innerHTML = strategy
+        h1.style.transform = `rotateX(${-degrees}deg)`;
+    }, 600);
 }
 
 const randomize = () => {
@@ -35,6 +40,9 @@ document.addEventListener('DOMContentLoaded', async () => {
 document.defaultView.addEventListener('keyup', (e) => {
     if (result.length === 0) return
     if (e.keyCode === 38) {
+        degrees += 180
+        card.style.transform = `rotateX(${degrees}deg)`;
+
         if (index < (drawn.length - 1)) {
             index++
             change(drawn[index])
@@ -50,8 +58,8 @@ document.defaultView.addEventListener('keyup', (e) => {
     if (index === 0) return
     if (e.keyCode === 40) {
         index--
+        degrees -= 180
+        card.style.transform = `rotateX(${degrees}deg)`;
         change(drawn[index])
     }
 })
-
-// HIDE MOUSE?
